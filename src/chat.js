144eadd7f55f10fd9a4944cfd301761c5977295d
nganguly0594 <neilganguly2015@gmail.com>
2024-03-22 14:25:41 -0500

    added nav bar to chat interface

--- src/chat.js
+++ src/chat.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 //import { getResponse } from "./llama";
+import Navigation from "./Navigation";
 
 const Chat = () => {
   const [userInput, setUserInput] = useState("");
@@ -14,6 +15,7 @@ const Chat = () => {
 
   return (
     <div>
+      <Navigation/>
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
